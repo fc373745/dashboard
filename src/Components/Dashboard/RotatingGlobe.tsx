1e1908dab638cba0b1572c0e175a5894efd05d5a
francis <fc373745@gmail.com>
2019-01-28 03:58:54 -0800

    going to remove globe and replace it with map

--- src/Components/Dashboard/RotatingGlobe.tsx
+++ src/Components/Dashboard/RotatingGlobe.tsx
@@ -5,10 +5,16 @@ import { feature } from "topojson";
 import world from "./world.json";
 
 const Global = createGlobalStyle`
+    body{
+        background-color: #141F1F;
+    }
     path{
-        fill:#aaa;
-        stroke: #ddd;
+        fill:#D1E0E0;
+        stroke: #0A1010;
         stroke-width: 0.5;
+
+        &:hover {
+        }
     }
 `;
 
@@ -18,9 +24,13 @@ const RotatingGlobe: React.FunctionComponent = () => {
     const [rotation, setRotation] = useState(0);
     const globeRef = React.createRef<SVGSVGElement>();
     //@ts-ignore
-    const geoJson = feature(world, world.objects.ne_110m_admin_0_countries);
+    const geoJson = feature(world, countries);
+    //@ts-ignore
+    const countries = feature(world, world.objects.ne_110m_admin_0_countries)
+        .features;
     const projection = geoOrthographic()
         .fitSize([450, 450], geoJson)
+        // .scale(((450 - 10) / 2) * 1.3)
         .rotate([rotation] as any);
 
     useEffect(() => {
@@ -28,15 +38,27 @@ const RotatingGlobe: React.FunctionComponent = () => {
             const geoGenerator = geoPath().projection(projection);
 
             setPath(geoGenerator(geoJson));
+
+            console.log(
+                //@ts-ignore
+                feature(world, world.objects.ne_110m_admin_0_countries).features
+            );
+
+            setMount(true);
         }
         setRotation(rotation + 0.2);
     });
 
+    const setCountry = () => {};
+
     return (
-        <svg width={500} height={500}>
-            <Global />
-            {pathString && <path d={pathString} />}
-        </svg>
+        <div>
+            <svg width={500} height={500}>
+                <Global />
+                {pathString && <path d={pathString} />}
+            </svg>
+            <button onClick={setCountry}>Click</button>
+        </div>
     );
 };
 
